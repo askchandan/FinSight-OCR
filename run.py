d02a695dc23dd4eca324e15a7a5dabc824abe8b3
askchandan <chandanmalakar6209@gmail.com>
2025-11-15 15:49:03 +0530

    Ingestion Standardised

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 from src.ingestion.loaders.image_loader import KaggleLoader
 
 
-loader = KaggleLoader(
-        dataset = 'mehaksingal/personal-financial-dataset-for-india',
-        extract_path = './data/extracted/'
-)
+loader = KaggleLoader()
 
 loader.download()
 data_path = loader.extract()
